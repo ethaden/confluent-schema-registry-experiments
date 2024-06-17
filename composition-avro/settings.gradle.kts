ad67d944e1f1d3ed34c67d0f91c52c8b3533e935
Eike Thaden <ethaden@confluent.io>
2024-06-17 12:51:14 +0200

    Renamed folder

---
+++ composition-avro/settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "kafka-schema-registry-experiments-inheritance"
+
+include("java-producer-with-schema-v1", "java-consumer-with-schema-v1")
--- inheritance/settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-rootProject.name = "kafka-schema-registry-experiments-inheritance"
-
-include("java-producer-v1", "java-consumer-v1")
