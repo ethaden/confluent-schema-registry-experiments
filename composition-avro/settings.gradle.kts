70fdb47dc9a9a28708cb6c4b073f20647cfcb50f
Eike Thaden <ethaden@confluent.io>
2024-07-01 19:16:49 +0200

    Fixed imports

--- composition-avro/settings.gradle.kts
+++ composition-avro/settings.gradle.kts
@@ -1,3 +1,8 @@
 rootProject.name = "kafka-schema-registry-experiments-inheritance"
 
-include("java-producer-with-schema-v1", "java-producer-with-schema-v2", "java-consumer-with-schema-v1", "java-consumer-with-reflection")
+include("java-producer-with-schema-v1", 
+    "java-producer-with-schema-v2", 
+    "java-consumer-with-schema-v1-specific-types", 
+    "java-consumer-with-schema-v1-generic-types",
+    "java-consumer-with-reflection"
+)
