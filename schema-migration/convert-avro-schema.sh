6fb167e595221a46bb37413d4c5a5e4571e63f93
Eike Thaden <ethaden@confluent.io>
2025-02-12 18:12:14 +0000

    advanced schema registry features

--- schema-migration/convert-avro-schema.sh
+++ schema-migration/convert-avro-schema.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 if [ -z "${1}" ]; then
-  echo "Usage: ${1} <schema-file>"
+  echo "Usage: ${1} <schema-file> [<application version>]"
   exit 1
 fi
+SCHEMA_FILE=${1}
+APPLICATION_VERSION=${2}
 
 #echo \\"schema\\": \\"" + " ".join(sys.stdin.readlines()).replace("\\n", "").replace("\\"", "\\\\\\"") + "\\"}\'")'
-SCHEMA="$(<${1})"
+SCHEMA="$(<${SCHEMA_FILE})"
 SCHEMA="${SCHEMA//\"/\\\"}"
 SCHEMA="${SCHEMA//[$'\t\r\n ']}"
-echo "{ \"schema\": \"$SCHEMA\" }"
+if [ -z ${APPLICATION_VERSION} ]; then
+  echo "{ \"schema\": \"$SCHEMA\" }"
+else
+  echo "{ \"schema\": \"$SCHEMA\", \"metadata\": { \"properties\": { \"major_version\": \"${APPLICATION_VERSION}\" } } }"
+fi
